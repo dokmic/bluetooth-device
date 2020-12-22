2ef65f97b112a72777023fe7912c5340eea29da7
Michael Dokolin <m@dokol.in>
2020-12-22 06:56:01 +0100

    Add Bluetooth device connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import PCancelable from 'p-cancelable';
 import noble, { Peripheral } from '@abandonware/noble';
-import { semaphore, timeout } from 'ts-async-decorators';
+import { before, semaphore, timeout } from 'ts-async-decorators';
 
 const DEFAULT_TIMEOUT_DISCOVERY = 30000;
+const DEFAULT_TIMEOUT = 10000;
 
 /**
  * Bluetooth Device Options.
@@ -13,6 +14,12 @@ interface BluetoothDeviceOptions {
    * By default, it equals 30 seconds.
    */
   discoveryTimeout?: number;
+
+  /**
+   * Device operations timeout, namely, connection, read, and write.
+   * By default, it equals 10 seconds.
+   */
+  timeout?: number;
 }
 
 /**
@@ -72,6 +79,44 @@ export class BluetoothDevice {
       if (noble.state === 'poweredOn') {
         await noble.startScanningAsync();
       }
+    });
+  }
+
+  /**
+   * Tries to establish a connection with the device.
+   * If the device was not previously discovered, the discovery operation will be performed before.
+   * @returns Connection promise.
+   */
+  @before({ action: (device: BluetoothDevice) => device.discover(), wait: true })
+  @timeout({
+    timeout(this: BluetoothDevice) {
+      return this.options.timeout ?? DEFAULT_TIMEOUT;
+    },
+    reason: 'Connection timeout.',
+  })
+  connect(): PCancelable<void> {
+    return new PCancelable((resolve, reject, onCancel) => {
+      if (this.peripheral?.state === 'connected') {
+        return resolve();
+      }
+
+      const onConnect = (error?: string) => (error ? reject(new Error(error)) : resolve());
+      const onDisconnect = () => this.peripheral?.connect(onConnect);
+
+      onCancel(() => {
+        this.peripheral?.removeListener('connect', onConnect);
+        this.peripheral?.removeListener('disconnect', onDisconnect);
+      });
+
+      if (this.peripheral?.state === 'connecting') {
+        return this.peripheral?.once('connect', onConnect);
+      }
+
+      if (this.peripheral?.state === 'disconnecting') {
+        return this.peripheral?.once('disconnect', onDisconnect);
+      }
+
+      return onDisconnect();
     });
   }
 }
